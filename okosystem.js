a667b5d461206d6deb0a6b7e4e9bd7d0a603ba55
Anders Tornøe <ato@kvuc.dk>
2016-11-28 12:15:20 +0100

    ryddet op i mappen og tilføjet genopretnings video funktionalitet

--- okosystem.js
+++ okosystem.js
@@ -1,16 +1,12 @@
 /*============================================
 =            Initialize variables            =
 ============================================*/
-var viewArray = [$(".balance_container"), $(".ubalance_container")],
-    /* array der holder div'erne indhold */
-
-    /*----------  hvilken tilstand : balance elle ublance?  ----------*/
-    state = 0,
+var state = 0,
 
     /*----------  hvor meget ubalance er der?  ----------*/
     ubalance_niveau = 0,
 
-
+    viewArray = [$(".balance_container"), $(".ubalance_container"), $(".genopretnings_container")],
     state_Array = ["balance_spm", "ubalance_spm"],
     /*----------  variabler til at tilgå json pba sø state  ----------*/
 
@@ -20,43 +16,38 @@ var viewArray = [$(".balance_container"), $(".ubalance_container")],
 
     korrekt_svar,
 
-    runder = [0, 0]; /*----------  hvilken runde spørgsmål er vi i?  ----------*/
+    runder = [0, 0, 0]; /*----------  hvilken runde spørgsmål er vi i?  ----------*/
 
 
 /*=====  End of Initialize variables  ======*/
 
 
 $(document).ready(function() {
-    viewArray[1].fadeOut(0);
     $('li').click(toggleView);
     poseQuestion(runder[state]);
 
-    init(state);
+    generate_labels(state);
 
-
-    $(".gif").click(function() {
-        var indeks = $(this).index(".gif");
-        show_info(indeks);
-    });
     $(".detalje_label").click(function() {
         var indeks = $(this).index(".detalje_label");
         show_info(indeks);
     });
 });
 
+/*----------  Subsection comment block  ----------*/
 
-function init(state) {
+function generate_labels(state) {
     //console.log(state);
-    for (var i = 0; i < jsonData.elementer.length; i++) {
-        var element = jsonData.elementer[i];
+    for (var i = 0; i < jsonData.labels.length; i++) {
+        var element = jsonData.labels[i];
         console.log(i + " punkt");
 
-        viewArray[state].append("<div><img class='gif' src=" + element.pics[state] + "></div>");
-        
-        viewArray[state].append("<span class='btn btn-xs btn-default detalje_label'><span class='glyphicon glyphicon-search'> </span> " + element.element + "</span>");
+        //viewArray[state].append("<div><img class='gif' src=" + element.pics[state] + "></div>");
+
+        $(".labels_container").append("<span class='btn btn-xs btn-default detalje_label'><span class='glyphicon glyphicon-search'> </span> " + element.element + "</span>");
 
         $(".detalje_label").eq(i).css("left", element.balance_pos[0] + "%").css("top", element.balance_pos[1] + "%")
-        $(".gif").eq(i).css("left", element.balance_pos[0] + "%").css("top", element.balance_pos[1] + "%");
+            //$(".gif").eq(i).css("left", element.balance_pos[0] + "%").css("top", element.balance_pos[1] + "%");
     }
     console.log("break");
 }
@@ -67,7 +58,14 @@ function init(state) {
 function show_info(indeks) {
     console.log("I: " + indeks);
     //$(".container-fluid").append("<div class='info_container'><div class='info_box'><h4>" + jsonData.elementer[indeks].element + "</h4><img class='infopic' src='" + jsonData.elementer[indeks].pic + "'><p>" + jsonData.elementer[indeks].infotekst + "</p></div></div>")
-    UserMsgBox("body", "<h3>" + jsonData.elementer[indeks].element + "</h3><img class='img-responsive' src='" + jsonData.elementer[indeks].pic + "'><p>" + jsonData.elementer[indeks].infotekst + "</p>");
+    if (state == 0) {
+        UserMsgBox("body", "<h3>" + jsonData.labels[indeks].element + ": Info</h3><img class='img-responsive' src='" + jsonData.labels[indeks].pics[0] + "'><p>" + jsonData.labels[indeks].infotekst + "</p>");
+    } else if (state == 1) {
+
+    } else if (state == 2) {
+        UserMsgBox("body", "<h3>" + jsonData.labels[indeks].element + ": Genopretning</h3><div class='embed-responsive embed-responsive-16by9'><iframe class='embed-responsive-item' src='https://www.youtube.com/embed/" + jsonData.labels[indeks].genopretning_url + "?rel=0'></iframe></div>");
+
+    }
 
     //viewArray[state].addClass("blur");
 }
@@ -93,35 +91,38 @@ function poseQuestion() {
     console.log("runder: " + runder);
     var spmData = jsonData[state_Array[state]];
 
-    if (runder[state] < spmData.length) {
+    if (state != 2) {
 
-        $(".spm_numbers").html("Spørgsmål " + (runder[state] + 1) + " / " + spmData.length);
-        $(".spm").html(spmData[runder[state]].spm);
+        if (runder[state] < spmData.length) {
 
-        svar_Array = [];
-        var svarHTML = "<div class='answerWrap'>";
+            $(".spm_numbers").html("Spørgsmål " + (runder[state] + 1) + " / " + spmData.length);
+            $(".spm").html(spmData[runder[state]].spm);
 
-        for (var i = 0; i < spmData[runder[state]].forkerte_svar.length; i++) {
-            svar_Array.push(spmData[runder[state]].forkerte_svar[i]);
+            svar_Array = [];
+            var svarHTML = "<div class='answerWrap'>";
+
+            for (var i = 0; i < spmData[runder[state]].forkerte_svar.length; i++) {
+                svar_Array.push(spmData[runder[state]].forkerte_svar[i]);
+            }
+            svar_Array.push(spmData[runder[state]].svar);
+
+            korrekt_svar = spmData[runder[state]].svar;
+            svar_Array = shuffle_Array(svar_Array);
+
+            for (var i = 0; i < svar_Array.length; i++) {
+                svarHTML += "<div class='answerChoice radioWrap'><label class='rdo_label'><input name='radioName' type='radio' value='" + i + "'><span class='svar_txt'>" + svar_Array[i] + " </span></label></div>"
+            }
+
+            $(".svar").html("</div>" + svarHTML + "<div class='btn btn-info btn_tjek'>Tjek svar</div>");
+
+            $(".btn_tjek").click(tjek_svar);
+        } else {
+            $(".spm").html("<h3>Du har besvaret alle spørgsmål <span class='label label-success'>korrekt</span>");
+            $(".svar").html("<div class='btn btn-info btn_forfra'>Tag quizzen igen</div>");
+            $(".btn_forfra").click(function() {
+                genstart_quiz();
+            });
         }
-        svar_Array.push(spmData[runder[state]].svar);
-
-        korrekt_svar = spmData[runder[state]].svar;
-        svar_Array = shuffle_Array(svar_Array);
-
-        for (var i = 0; i < svar_Array.length; i++) {
-            svarHTML += "<div class='answerChoice radioWrap'><label class='rdo_label'><input name='radioName' type='radio' value='" + i + "'><span class='svar_txt'>" + svar_Array[i] + " </span></label></div>"
-        }
-
-        $(".svar").html("</div>" + svarHTML + "<div class='btn btn-info btn_tjek'>Tjek svar</div>");
-
-        $(".btn_tjek").click(tjek_svar);
-    } else {
-        $(".spm").html("<h3>Du har besvaret alle spørgsmål <span class='label label-success'>korrekt</span>");
-        $(".svar").html("<div class='btn btn-info btn_forfra'>Tag quizzen igen</div>");
-        $(".btn_forfra").click(function() {
-            genstart_quiz();
-        });
     }
 }
 
@@ -163,7 +164,7 @@ function feedback(svar) {
         if (svar == true) {
             UserMsgBox("body", "<h3>Du har svaret <span class='label label-success'>korrekt</span></h3>");
         } else {
-            UserMsgBox("body", "<h3>Du har svaret <span class='label label-danger'>forkert</span></h3>");
+            UserMsgBox("body", "<h3>Du har svaret <span class='label label-danger'>forkert</span></h3><p>Klik på de enkelte elementer og lær mere om dem.</p>");
         }
     } else {
         if (svar == true) {
